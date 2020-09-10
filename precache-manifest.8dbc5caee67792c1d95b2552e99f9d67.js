409ba943114939bb2aaa1447bfab8d651afe584a
Pierre C <se.scez@gmail.com>
2020-09-10 19:39:49 +0200

    Updates

--- precache-manifest.8dbc5caee67792c1d95b2552e99f9d67.js
+++ precache-manifest.8dbc5caee67792c1d95b2552e99f9d67.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7e7505a42224f80c91e86eefd330beb",
+    "revision": "b54262ceb04d74055404e4528c399e35",
     "url": "/Sudoku-Solver/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sudoku-Solver/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "955e648aac84a77e2db8",
+    "revision": "62ba011012500c747e79",
     "url": "/Sudoku-Solver/static/css/main.3b454b3e.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sudoku-Solver/static/js/2.a0873241.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "955e648aac84a77e2db8",
-    "url": "/Sudoku-Solver/static/js/main.72eb0f34.chunk.js"
+    "revision": "62ba011012500c747e79",
+    "url": "/Sudoku-Solver/static/js/main.1ede2f14.chunk.js"
   },
   {
     "revision": "8a8e6d76fd5c5b5302ed",
